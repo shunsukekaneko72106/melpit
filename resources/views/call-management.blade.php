0a0105b08a18bdf8fe702df84468678110bd860d
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-05-03 09:00:31 +0900

    ボタン表示

--- resources/views/call-management.blade.php
+++ resources/views/call-management.blade.php
@@ -6,15 +6,15 @@
 
 */
 
-
-
 ?>
 <!doctype html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('/css/call-management.blade.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/call-management.css') }}">
+        <script src="{{ asset('/js/call-management.js') }}"></script>
+
 
         <title>架電管理画面</title>
     </head>
